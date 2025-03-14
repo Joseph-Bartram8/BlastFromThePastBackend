3bef7810a2b81a9677f0a1f0f6c28cae6b9ee45e
joseph_bartram8 <josephbartram@outlook.com>
2025-03-14 13:38:17 +0000

    more changes to cors.go, local still working

--- vintage-toy-api/middleware/cors.go
+++ vintage-toy-api/middleware/cors.go
@@ -4,10 +4,22 @@ import (
 	"net/http"
 )
 
+// Allowed origins for local and live environments
+var allowedOrigins = map[string]bool{
+	"http://localhost:5173":           true,
+	"https://toy.josephbartram.co.uk": true,
+}
+
 // CORS Middleware to Allow Frontend Requests
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173, https://toy.josephbartram.co.uk")
+		origin := r.Header.Get("Origin")
+
+		// Check if the origin is in our allowed list
+		if _, ok := allowedOrigins[origin]; ok {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Vary", "Origin")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
